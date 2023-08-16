786f89b9504c6cb0a5f1a219f14abe6b8f281983
Kevin Tong <kevintong@Kevins-MacBook-Pro.local>
2023-08-16 11:54:24 -0700

    Added basic tests for the circular array deque

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "list_deque.h"
+#include "circular_deque.h"
 #include <iostream>
 
 int main() {
@@ -7,5 +8,11 @@ int main() {
     q.push_back(3);
     q.push_back(10);
     std::cout << q.front() << ", " << q[1] << ", " << q.back() << "\n";
+
+    deques::CircularDeque<int> cq;
+    cq.push_front(2);
+    cq.push_back(3);
+    cq.push_back(10);
+    std::cout << cq.front() << ", " << cq[1] << ", " << cq.back() << "\n";
     return 0;
 }
