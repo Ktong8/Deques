57749958f8d1a37e4296a3ed62b7ef2f449d0045
Kevin Tong <kevintong@Kevins-MacBook-Pro.local>
2023-08-16 12:28:40 -0700

    Made the tests for the list deque and circular deque more comprehensive

--- main.cpp
+++ main.cpp
@@ -8,11 +8,17 @@ int main() {
     q.push_back(3);
     q.push_back(10);
     std::cout << q.front() << ", " << q[1] << ", " << q.back() << "\n";
+    q.pop_front();
+    q.pop_back();
+    std::cout << q.front() << ", " << q.back() << ", " << q[0] << "\n";
 
     deques::CircularDeque<int> cq;
     cq.push_front(2);
     cq.push_back(3);
     cq.push_back(10);
     std::cout << cq.front() << ", " << cq[1] << ", " << cq.back() << "\n";
+    cq.pop_front();
+    cq.pop_back();
+    std::cout << cq.front() << ", " << cq.back() << ", " << cq[0] << "\n";
     return 0;
 }
