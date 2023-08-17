cbe096417679b08f6c8212cd155953584fccdcb1
Kevin Tong <kevintong@Kevins-MacBook-Pro.local>
2023-08-17 09:06:52 -0700

    Modularized the test function and added benchmarks for the different deque implementations

--- main.cpp
+++ main.cpp
@@ -1,11 +1,35 @@
 #include "list_deque.h"
 #include "circular_deque.h"
 #include "array_deque.h"
+#include "timer.h"
 #include <iostream>
+#include <deque>
 
-int main() {
-    std::cout << "List Deque\n";
-    deques::ListDeque<int> q;
+template <typename T>
+void simulateDeque() {
+    T q;
+    for (int i = 0; i < 62000; ++i) {
+        if (i % 3) {
+            q.push_front(i);
+        } else {
+            q.push_back(i);
+        }
+        if (!(i % 20)) {
+            q.pop_front();
+        } else if (!(i % 21)) {
+            q.pop_back();
+        }
+    }
+    int sum = 0;
+    for (int i = 0; i < 20; ++i) {
+        sum += q[i];
+    }
+}
+
+
+template <typename T>
+void testDeque() {
+    T q;
     q.push_front(2);
     q.push_back(3);
     q.push_back(10);
@@ -13,25 +37,13 @@ int main() {
     q.pop_front();
     q.pop_back();
     std::cout << q.front() << ", " << q.back() << ", " << q[0] << "\n";
+}
 
-    std::cout << "Circular Deque\n";
-    deques::CircularDeque<int> cq;
-    cq.push_front(2);
-    cq.push_back(3);
-    cq.push_back(10);
-    std::cout << cq.front() << ", " << cq[1] << ", " << cq.back() << "\n";
-    cq.pop_front();
-    cq.pop_back();
-    std::cout << cq.front() << ", " << cq.back() << ", " << cq[0] << "\n";
 
-    std::cout << "Array Deque\n";
-    deques::ArrayDeque<int> aq;
-    aq.push_front(2);
-    aq.push_back(3);
-    aq.push_back(10);
-    std::cout << aq.front() << ", " << aq[1] << ", " << aq.back() << "\n";
-    aq.pop_front();
-    aq.pop_back();
-    std::cout << aq.front() << ", " << aq.back() << ", " << aq[0] << "\n";
+int main() {
+    timer::timeFunction(simulateDeque<deques::ListDeque<int>>, "List Deque");
+    timer::timeFunction(simulateDeque<deques::CircularDeque<int>>, "Circular Deque");
+    timer::timeFunction(simulateDeque<deques::ArrayDeque<int>>, "Array Deque");
+    timer::timeFunction(simulateDeque<std::deque<int>>, "Standard Deque");
     return 0;
 }
