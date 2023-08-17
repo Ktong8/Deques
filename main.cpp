2b83cbb506e77ad3a581c1e6a7307b2e07c751bc
Kevin Tong <kevintong@Kevins-MacBook-Pro.local>
2023-08-16 17:40:50 -0700

    Added a basic test for the array of arrays implementation of deque

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include "list_deque.h"
 #include "circular_deque.h"
+#include "array_deque.h"
 #include <iostream>
 
 int main() {
+    std::cout << "List Deque\n";
     deques::ListDeque<int> q;
     q.push_front(2);
     q.push_back(3);
@@ -12,6 +14,7 @@ int main() {
     q.pop_back();
     std::cout << q.front() << ", " << q.back() << ", " << q[0] << "\n";
 
+    std::cout << "Circular Deque\n";
     deques::CircularDeque<int> cq;
     cq.push_front(2);
     cq.push_back(3);
@@ -20,5 +23,15 @@ int main() {
     cq.pop_front();
     cq.pop_back();
     std::cout << cq.front() << ", " << cq.back() << ", " << cq[0] << "\n";
+
+    std::cout << "Array Deque\n";
+    deques::ArrayDeque<int> aq;
+    aq.push_front(2);
+    aq.push_back(3);
+    aq.push_back(10);
+    std::cout << aq.front() << ", " << aq[1] << ", " << aq.back() << "\n";
+    aq.pop_front();
+    aq.pop_back();
+    std::cout << aq.front() << ", " << aq.back() << ", " << aq[0] << "\n";
     return 0;
 }
